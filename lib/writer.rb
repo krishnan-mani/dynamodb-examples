516de00d0f0257adff8c3287e364b1dc4758f002
Krishnan Mani <km@krishnanm.com>
2016-03-15 23:27:14 +0530

    Now saves items to both hash and hash-and-range-key tables

--- lib/writer.rb
+++ lib/writer.rb
@@ -4,19 +4,31 @@ class Writer
 
   def initialize(connection_info)
     @client = Aws::DynamoDB::Client.new(connection_info)
-    ensure_tables
   end
 
-  def save(item)
-    @client.put_item({
-                         table_name: 'foo',
-                         item: item
-                     })
+  def save_foo(*items)
+    ensure_foo_table
+    items.each do |item|
+      @client.put_item({
+                           table_name: 'foo',
+                           item: item
+                       })
+    end
+  end
+
+  def save_bar(*items)
+    ensure_bar_table
+    items.each do |item|
+      @client.put_item({
+                           table_name: 'bar',
+                           item: item
+                       })
+    end
   end
 
   private
 
-  def ensure_tables
+  def ensure_foo_table
     tables = @client.list_tables.table_names
     unless tables.include?('foo')
       @client.create_table({
@@ -31,6 +43,39 @@ class Writer
                                    {
                                        attribute_name: 'k1',
                                        key_type: 'HASH'
+                                   }
+                               ],
+                               provisioned_throughput: {
+                                   read_capacity_units: 1,
+                                   write_capacity_units: 1
+                               }
+                           })
+    end
+  end
+
+  def ensure_bar_table
+    tables = @client.list_tables.table_names
+    unless tables.include?('bar')
+      @client.create_table({
+                               table_name: 'bar',
+                               attribute_definitions: [
+                                   {
+                                       attribute_name: 'k1',
+                                       attribute_type: 'S'
+                                   },
+                                   {
+                                       attribute_name: 'k2',
+                                       attribute_type: 'S'
+                                   }
+                               ],
+                               key_schema: [
+                                   {
+                                       attribute_name: 'k1',
+                                       key_type: 'HASH'
+                                   },
+                                   {
+                                       attribute_name: 'k2',
+                                       key_type: 'RANGE'
                                    }
                                ],
                                provisioned_throughput: {
--- spec/write_items_spec.rb
+++ spec/write_items_spec.rb
@@ -2,32 +2,50 @@ require_relative '../lib/writer'
 
 require 'aws-sdk'
 
+require 'byebug'
+
 RSpec.describe Writer do
   connection_info = {:region => 'us-east-1', :endpoint => 'http://localhost:8000'}
 
-  it 'saves a rich document to DynamoDB' do
+  it 'saves a rich document by hash key to DynamoDB' do
     item = {
-        'k1': 'v1',
-        'k2': 23,
-        'k3': true,
-        'k4': [1, 2, 3],
-        'k5': {
+        k1: 'v1',
+        k2: 23,
+        k3: true,
+        k4: [1, 2, 3],
+        k5: {
             'k51': 'abc',
             'k52': 'def'
         }
     }
-    Writer.new(connection_info).save(item)
+    Writer.new(connection_info).save_foo(item)
 
     client = Aws::DynamoDB::Client.new(connection_info)
     response = client.get_item({
                                    table_name: 'foo',
                                    key: {
-                                       'k1': 'v1'
+                                       k1: 'v1'
                                    }
 
                                })
 
     expect(response.item[:k1]).to eql(item['k1'])
     expect(response.item[:k2]).to eql(item['k2'])
+  end
+
+  it 'saves multiple documents by hash and range keys to DynamoDB' do
+    items = [{k1: 'a1', k2: 'a2'}, {k1: 'a1', k2: 'y'}]
+    Writer.new(connection_info).save_bar(*items)
+
+    client = Aws::DynamoDB::Client.new(connection_info)
+    response = client.query({
+                                table_name: 'bar',
+                                select: 'COUNT',
+                                key_condition_expression: 'k1 = :v_k1',
+                                expression_attribute_values: {
+                                    ':v_k1': 'a1'
+                                }
+                            })
+    expect(response.count).to eql(2)
   end
 end
