084c2b4cfc021918934d37836f7768b455cd24fd
Krishnan Mani <km@krishnanm.com>
2016-04-28 20:08:39 +0530

    Added another illustration to query items using global storage index

--- spec/global_secondary_index_spec.rb
+++ spec/global_secondary_index_spec.rb
@@ -2,6 +2,143 @@ require 'aws-sdk'
 
 RSpec.describe 'global secondary indexes' do
 
+  it 'use a global secondary index to locate items when the hash key values are not known', :focus => true do
+
+    instance_states = [
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T12:01:23+05:30',
+            'instance_state': 'pending',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-87999def',
+            'at_time': '2016-03-29T12:02:37+05:30',
+            'instance_state': 'running',
+            'Name': 'api-2'
+        },
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T12:01:23+05:30',
+            'instance_state': 'running',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T17:01:23+05:30',
+            'instance_state': 'stopping',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T18:01:23+05:30',
+            'instance_state': 'rebooting',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T19:01:23+05:30',
+            'instance_state': 'shutting-down',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-12345abc',
+            'at_time': '2016-03-29T19:01:23+05:30',
+            'instance_state': 'terminated',
+            'Name': 'api-1'
+        },
+        {
+            'instance_id': 'i-87654def',
+            'at_time': '2016-03-30T03:42:19+05:30',
+            'instance_state': 'terminated',
+            'Name': 'api-2'
+        },
+    ]
+
+    client = Aws::DynamoDB::Client.new(connection_info)
+    delete_table('instance_states')
+    client.create_table({
+                            table_name: 'instance_states',
+                            attribute_definitions: [
+                                {
+                                    attribute_name: 'instance_id',
+                                    attribute_type: 'S'
+                                },
+                                {
+                                    attribute_name: 'at_time',
+                                    attribute_type: 'S'
+                                },
+                                {
+                                    attribute_name: 'instance_state',
+                                    attribute_type: 'S'
+                                }
+                            ],
+                            key_schema: [
+                                {
+                                    attribute_name: 'instance_id',
+                                    key_type: 'HASH'
+                                },
+                                {
+                                    attribute_name: 'at_time',
+                                    key_type: 'RANGE'
+                                }
+                            ],
+                            global_secondary_indexes: [
+                                {
+                                    index_name: 'gsi_instance_state',
+                                    key_schema: [
+                                        {
+                                            attribute_name: 'instance_state',
+                                            key_type: 'HASH'
+                                        },
+                                        {
+                                            attribute_name: 'at_time',
+                                            key_type: 'RANGE'
+                                        }
+                                    ],
+                                    projection: {
+                                        projection_type: 'ALL'
+                                    },
+                                    provisioned_throughput: {
+                                        read_capacity_units: 1,
+                                        write_capacity_units: 1
+                                    }
+                                }
+                            ],
+                            provisioned_throughput: {
+                                read_capacity_units: 1,
+                                write_capacity_units: 1
+                            }
+                        })
+
+    instance_states.each do |state|
+      client.put_item({
+                          table_name: 'instance_states',
+                          item: state
+                      })
+    end
+
+    #Retrieve instance state information for instances in a certain state
+    response = client.query({
+                                table_name: 'instance_states',
+                                index_name: 'gsi_instance_state',
+                                key_condition_expression: 'instance_state = :v_state AND at_time >= :v_at_time',
+                                expression_attribute_values: {
+                                    ':v_state': 'running',
+                                    ':v_at_time': '2016-03-29T12:01:23+05:30'
+                                }
+                            })
+
+    running_instance_states = response.items
+    expect(running_instance_states).not_to be_empty
+    running_instances = response.items.collect do |instance_state|
+      instance_state['instance_id']
+    end
+    expect(running_instances.uniq).to match_array(['i-12345abc', 'i-87999def'])
+
+    #TODO: Retrieve the most recent state for an instance
+  end
+
   it 'create and query a global secondary index' do
     tournament_wins = {
         reggio_emilia_93: {
@@ -118,14 +255,14 @@ RSpec.describe 'global secondary indexes' do
     end
 
     karpov_wins_response = client.query({
-                                           table_name: 'tournaments',
-                                           index_name: 'gsi_name',
-                                           key_condition_expression: 'first_name = :v_fn AND last_name = :v_ln',
-                                           expression_attribute_values: {
-                                               ':v_fn': 'Anatoly',
-                                               ':v_ln': 'Karpov'
-                                           }
-                                       })
+                                            table_name: 'tournaments',
+                                            index_name: 'gsi_name',
+                                            key_condition_expression: 'first_name = :v_fn AND last_name = :v_ln',
+                                            expression_attribute_values: {
+                                                ':v_fn': 'Anatoly',
+                                                ':v_ln': 'Karpov'
+                                            }
+                                        })
     expect(karpov_wins_response.items).not_to be_empty
     expect(karpov_wins_response.items.first['tournament']).to eql('Hoogovens 1993')
   end
